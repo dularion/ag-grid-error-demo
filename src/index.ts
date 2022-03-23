dbca1034180fb3753f9bbc804eba04b729205e7a
Antonia Engfors <antonia@systaro.de>
2022-03-23 22:28:03 +0100

    add ag-grid error demo for sass issue

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './styles.scss';
+
+
+let enterprise = require('@ag-grid-enterprise/core');
+enterprise.LicenseManager.setLicenseKey('CompanyName=Systaro GmbH,LicensedApplication=basex,LicenseType=SingleApplication,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=1,AssetReference=AG-026444,ExpiryDate=18_March_2023_[v2]_MTY3OTA5NzYwMDAwMA==66376a896b7c2f96213ab836d408b69a');
+
+
+import '@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css';
+import '@ag-grid-enterprise/all-modules/dist/styles/ag-theme-alpine.css';
